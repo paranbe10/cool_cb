11680bbcfa22c00111451e0b7369fddc529f6984
paranbe10 <paranbe10@gmail.com>
2026-06-03 14:23:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ css_style = """
     .chat-container {
         display: flex;
         flex-direction: column;
-        gap: 12px;
+        gap: 25px;
         margin-bottom: 20px;
         width: 100%;
     }
@@ -46,7 +46,7 @@ css_style = """
     .ai-msg {
         background-color: #ffffff;
         color: #333333;
-        border-top-left-radius: 10px;
+        border-top-left-radius: 0px;
         border: 1px solid #e2e2e2;
     }
 </style>
